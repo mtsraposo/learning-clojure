f6512553d4b13d38f50bc763168f905623da1704
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-19 21:33:47 -0300

    add symmetrize

---
+++ test/learning_clojure/symmetrize_test.clj
@@ -0,0 +1,27 @@
+(ns learning-clojure.symmetrize-test
+  (:require [clojure.test :refer :all]
+            [learning-clojure.symmetrize :refer :all]))
+
+(deftest matching-part-test
+  (testing "Match single part"
+    (is (=
+          (matching-part {:name "left-eye" :size 1})
+          {:name "right-eye" :size 1}
+          )
+        )
+    )
+  )
+
+(deftest symmetrize-test
+  (testing "Symmetrize all body parts"
+    (is (=
+          (symmetrize-body-parts [{:name "head" :size 3}
+                                  {:name "left-eye" :size 1}]
+                                 )
+          [{:name "head" :size 3}
+           {:name "left-eye" :size 1}
+           {:name "right-eye" :size 1}]
+          )
+        )
+    )
+  )
